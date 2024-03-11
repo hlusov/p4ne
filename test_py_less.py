71978f9667426cd4141cbc0023d2598f3d7defc2
hlusov <hlusovserg@gmail.com>
2024-03-12 00:06:21 +0300

    коммент =4

--- test_py_less.py
+++ test_py_less.py
@@ -1,3 +1,12 @@
-import openpyxl as xl, re
+from openpyxl import Workbook as xl
+import re, random
 test_dir = 'C:\\Users\\laser\\Desktop\\'
 testfile = 'py_xls.xls'
+
+book = xl()
+ws = book.active
+color = str(hex(random.randint(0, 16777215)))[2:]
+print (color)
+ws.sheet_properties.tabColor = color
+
+book.save(testfile)
