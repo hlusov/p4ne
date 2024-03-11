12621b173ed998b8e6fceb8731e500694de80207
hlusov <hlusovserg@gmail.com>
2024-03-12 00:48:21 +0300

    коммент =4

--- test_py_less.py
+++ test_py_less.py
@@ -1,12 +1,20 @@
-from openpyxl import Workbook as xl
 import re, random
-test_dir = 'C:\\Users\\laser\\Desktop\\'
-testfile = 'py_xls.xls'
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 
-book = xl()
+test_dir = 'C:\\Users\\laser\\Desktop\\'
+testfile = 'py_xls.xlsx'
+
+book = Workbook()
 ws = book.active
 color = str(hex(random.randint(0, 16777215)))[2:]
 print (color)
 ws.sheet_properties.tabColor = color
+
+for row in ws.iter_rows(min_row=1, max_col=5, max_row=5):
+    for cell in row:
+        color = str(hex(random.randint(0, 16777215)))[2:]
+        cell.value = str(color)
+        cell.fill = PatternFill('solid', fgColor=color)
 
 book.save(testfile)
