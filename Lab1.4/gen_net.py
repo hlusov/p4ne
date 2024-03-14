bf32dbd01e660d81fd6a78f18e9f4d79a7dbdbfb
hlusov <hlusovserg@gmail.com>
2024-03-14 21:00:06 +0300

    no comments

--- Lab1.4/gen_net.py
+++ Lab1.4/gen_net.py
@@ -1,25 +1,33 @@
 import random, ipaddress
 from openpyxl import Workbook
+import matplotlib.pyplot as plot
 
 def gen_net():
     return ipaddress.IPv4Network((random.randint(int(ipaddress.IPv4Address('11.0.0.0')), int(ipaddress.IPv4Address('223.0.0.0'))), random.randint(8,24)), strict=False)
 
 i, k = 0, 0
 list_net = []
+graf = []
+n = 1
+for k in range (n):
+    i = 0
+    while i < 50:
+        k += 1
+        new_net = gen_net()
+        if not new_net.is_private:
+            list_net.append(new_net)
+            i += 1
+    graf.append(k)
 
-while i < 50:
-    k += 1
-    new_net = gen_net()
-    if new_net.is_private:
-        list_net.append(new_net)
-        i += 1
 
 list1 = sorted(list_net)
 print(k, i, list_net)
-
+print (graf)
 wb = Workbook()
 ws = wb.active
 
+plot.plot (range(n),graf)
+plot.show()
 # ws.append(list_net)
 #for i in len(list_net):
 #    ws[]
