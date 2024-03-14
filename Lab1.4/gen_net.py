acbae11a2687b73a6393343a04eff634ea8b25d9
hlusov <hlusovserg@gmail.com>
2024-03-14 20:29:36 +0300

    no comments

--- Lab1.4/gen_net.py
+++ Lab1.4/gen_net.py
@@ -4,20 +4,24 @@ from openpyxl import Workbook
 def gen_net():
     return ipaddress.IPv4Network((random.randint(int(ipaddress.IPv4Address('11.0.0.0')), int(ipaddress.IPv4Address('223.0.0.0'))), random.randint(8,24)), strict=False)
 
-i = 0
+i, k = 0, 0
 list_net = []
 
 while i < 50:
+    k += 1
     new_net = gen_net()
     if new_net.is_private:
         list_net.append(new_net)
         i += 1
 
-print(list_net)
+list1 = sorted(list_net)
+print(k, i, list_net)
 
 wb = Workbook()
 ws = wb.active
 
-ws.append(list_net)
+# ws.append(list_net)
+#for i in len(list_net):
+#    ws[]
 
 wb.save('nets.xlsx')
