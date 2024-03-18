ae774361053508e3dabb43b6abe63d00d6c97165
hlusov <hlusovserg@gmail.com>
2024-03-18 13:42:54 +0300

    no comments

--- Lab1.5/py_files.py
+++ Lab1.5/py_files.py
@@ -3,6 +3,14 @@ import glob
 PATH = 'C:\\Users\\laser\\Downloads\\config_files\\config_files\\'
 
 files = glob.glob(PATH + "*.log")
+iplist = set()
 
 for file in files:
-    print(file)
+    with(open(file)) as f:
+        for line in f:
+            if line.find("ip address") == 1:
+                iplist.add(line.replace("ip address", "").strip())
+
+for i in iplist:
+    print(i, sep=",")
+
---
+++ my exercises/summ.py
@@ -0,0 +1,6 @@
+
+
+a = input('enter N:')
+summ = 0
+for i in list(str(a)): summ += int(i)
+print(summ)
